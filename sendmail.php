911cf5ac5489b659ea832251f8b390b06fdf00da
babikern90-design <babikern90@gmail.com>
2026-03-13 15:16:03 +0100

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,38 @@
+<?php
+
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+require 'PHPMailer-master/src/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+
+try {
+
+// Configuration SMTP Mailtrap
+$mail->isSMTP();
+$mail->Host = 'sandbox.smtp.mailtrap.io';
+$mail->SMTPAuth = true;
+$mail->Username = '7c5d48e2db1f18';
+$mail->Password = 'a1ef4ec80504c6';
+$mail->Port = 2525;
+
+// Expéditeur et destinataire
+$mail->setFrom('monprojet13@outlook.fr', 'Projet Orientation');
+$mail->addAddress('test@test.com');
+
+// Contenu du mail
+$mail->isHTML(true);
+$mail->Subject = 'Test Projet Orientation';
+$mail->Body = 'Email envoyé avec PHPMailer et Mailtrap';
+
+$mail->send();
+echo "Email envoyé avec succès";
+
+} catch (Exception $e) {
+echo "Erreur : {$mail->ErrorInfo}";
+}
+
+?>
