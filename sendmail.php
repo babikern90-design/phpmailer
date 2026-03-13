7e0c07b9459568e69e7f1ff5ef9c03d4edbcf0cd
babikern90-design <babikern90@gmail.com>
2026-03-13 15:40:08 +0100

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,31 @@
+<?php
+
+// début PHP
+function connexpdo($base,$param) // fonction connexion
+{
+  include_once($param.".inc.php"); // fichier config
+
+  $dsn="mysql:host=".HOST.";dbname=".$base; // chaine connexion
+
+  $user=USER; // utilisateur BD
+
+  $pass=PASS; // mot passe
+
+  try // tester connexion
+  {
+    $idcom = new PDO($dsn,$user,$pass); // connexion PDO
+
+    return $idcom; // retourne connexion
+  }
+
+  catch(PDOException $except) // erreur PDO
+  {
+    echo "Echec de la connexion",$except->getMessage(); // affiche erreur
+
+    return FALSE; // retourne faux
+
+    exit(); // arrêt script
+  }
+}
+
+?> //fin php
